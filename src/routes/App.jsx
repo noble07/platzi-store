01176f719d89a68694965a829a39f1b43246b612
Juan Camilo <juancmz98@gmail.com>
2021-06-25 14:05:33 -0500

    Added Axios to project

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,17 +4,32 @@ import Home from '../containers/Home';
 import Checkout from '../containers/Checkout';
 import Layout from '../components/Layout';
 import NotFound from '../containers/NotFound';
+import useInitialState from '../hooks/useInitialState';
+import AppContext from '../context/AppContext';
 
-const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  </BrowserRouter>
-);
+const App = () => {
+  const { products } = useInitialState();
+  const isEmpty = products.length;
+
+  return (
+    <>
+      {isEmpty > 0 ? (
+        <AppContext.Provider value={products}>
+          <BrowserRouter>
+            <Layout>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/checkout" component={Checkout} />
+                <Route component={NotFound} />
+              </Switch>
+            </Layout>
+          </BrowserRouter>
+        </AppContext.Provider>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </>
+  );
+};
 
 export default App;
